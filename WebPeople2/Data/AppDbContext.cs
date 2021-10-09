a883ad0cac7b00746b4fd0b8fcc0fe42f0e794a2
Danny <a2013114435@estudiantes.upsa.edu.bo>
2021-10-09 07:51:47 -0400

    Agregar archivos de proyecto.

---
+++ WebPeople2/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebPeople2.Models;
+
+namespace WebPeople2.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Person> People { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        {
+
+        }
+    }
+}
---
+++ WebPeople2/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebPeople2.Models
+{
+    public enum SexType
+    {
+      Female = 0,
+      Male =1,
+    }
+    public class Person
+    {
+        [Key]
+        public int PersonId { get; set; }
+        [Required(ErrorMessage ="Name is required")]
+        [StringLength(60, MinimumLength = 5, ErrorMessage ="El nombre debe contener entre 5 a 60 caracteres")]
+        [Display(Name="Nombre completo de la persona")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name= "Sexo")]
+        public SexType Sex { get; set; }
+        [Display(Name= "Pasatiempos")]
+        public string Hobbie { get; set; }
+ 
+    }
+}
